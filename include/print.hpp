963236f7dc23195713eb68f30a39704945870edd
Link-God <panzar24@bk.ru>
2018-04-19 12:29:38 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,8 @@
 #include <fstream>
 #include <iostream>
 
+//Prints text to standard output stream
 void print(const std::string& text, std::ostream& out = std::cout);
+
+//Prints text to specified file
 void print(const std::string& text, std::ofstream& out);
